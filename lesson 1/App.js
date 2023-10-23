183887cf7d4da09c161dbae0fa59782f66c9f9eb
anoopraju31 <anoopraju31@gmail.com>
2023-10-23 13:50:05 +0530

    H1 in react - App.js - heading id

--- lesson 1/App.js
+++ lesson 1/App.js
@@ -1,4 +1,8 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const heading1 = React.createElement('h1', {}, 'Hello World from React')
+const heading1 = React.createElement(
+	'h1',
+	{ id: 'heading' },
+	'Hello World from React',
+)
 
 root.render(heading1)
