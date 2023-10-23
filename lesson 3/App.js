bc7d60c07cd5b2bcff1c29d2e40b78ecdbd780f3
anoopraju31 <anoopraju31@gmail.com>
2023-10-24 01:25:28 +0530

    JSX

--- lesson 3/App.js
+++ lesson 3/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement(
-	'h1',
-	{ id: 'heading' },
-	'Namaste JavaScript',
+// React Element - Object
+const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React')
+
+console.log(heading)
+
+// JSX is not HTML
+// JSX - HTML link or XML like syntax
+const jsxHeading = (
+	<h1 className='head' tabIndex={3}>
+		{' '}
+		Namaste React using JSX 🚀{' '}
+	</h1>
 )
+
+console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
+root.render(jsxHeading)
