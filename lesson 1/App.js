e06a682604019a31fc00a99c99f1feaa2a654de2
anoopraju31 <anoopraju31@gmail.com>
2023-10-23 13:57:43 +0530

    H1 in react - logging createElement()

--- lesson 1/App.js
+++ lesson 1/App.js
@@ -3,6 +3,8 @@ const heading1 = React.createElement(
 	'h1',
 	{ id: 'heading' },
 	'Hello World from React',
-)
+) //returns Object
 
-root.render(heading1)
+console.log(heading1)
+
+root.render(heading1) //render() => converts the js object into html h1 tag
