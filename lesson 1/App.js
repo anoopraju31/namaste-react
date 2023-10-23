74ef3f9090e137f354a5d36ee456b51df8c5e171
anoopraju31 <anoopraju31@gmail.com>
2023-10-23 14:11:25 +0530

    multiple child elements - reactjs

--- lesson 1/App.js
+++ lesson 1/App.js
@@ -1,8 +1,12 @@
 /*
     <div id="parent">
-        <div id="child">
-            <h1> Hello World </h1>
-            <h2> How Are You? </h2>
+        <div id="child1">
+            <h1> Hello World 1 </h1>
+            <h2> How Are You? 1 </h2>
+        </div>
+        <div id="child2">
+            <h1> Hello World 2 </h1>
+            <h2> How Are You? 2 </h2>
         </div>
     </div>
 */
@@ -10,11 +14,18 @@
 const parent = React.createElement(
 	'div', // element type
 	{ id: 'parent' }, //Props
-	React.createElement('div', { id: 'child' }, [
-		//children
-		React.createElement('h1', {}, 'Hello World'),
-		React.createElement('h2', {}, 'How Are You?'),
-	]),
+	[
+		React.createElement('div', { id: 'child1' }, [
+			//children
+			React.createElement('h1', {}, 'Hello World 1'),
+			React.createElement('h2', {}, 'How Are You? 1'),
+		]),
+		React.createElement('div', { id: 'child2' }, [
+			//children
+			React.createElement('h1', {}, 'Hello World 2'),
+			React.createElement('h2', {}, 'How Are You? 2'),
+		]),
+	],
 )
 
 console.log(parent) //Object
