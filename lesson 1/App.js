95ea2c741c2eeeb49e265a0d7ac51afb32abc03d
anoopraju31 <anoopraju31@gmail.com>
2023-10-23 14:07:37 +0530

    multiple element in same level - children will be an array in react

--- lesson 1/App.js
+++ lesson 1/App.js
@@ -2,18 +2,19 @@
     <div id="parent">
         <div id="child">
             <h1> Hello World </h1>
+            <h2> How Are You? </h2>
         </div>
     </div>
 */
 
 const parent = React.createElement(
-	'div',
-	{ id: 'parent' },
-	React.createElement(
-		'div',
-		{ id: 'child' },
+	'div', // element type
+	{ id: 'parent' }, //Props
+	React.createElement('div', { id: 'child' }, [
+		//children
 		React.createElement('h1', {}, 'Hello World'),
-	),
+		React.createElement('h2', {}, 'How Are You?'),
+	]),
 )
 
 console.log(parent) //Object
