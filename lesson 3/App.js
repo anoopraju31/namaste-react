84a1358221702aa545ac0c4eaa29f97705b83268
anoopraju31 <anoopraju31@gmail.com>
2023-10-24 00:20:10 +0530

    create element - initial

--- lesson 3/App.js
+++ lesson 3/App.js
@@ -1,27 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement(
-	'div', // element type
-	{ id: 'parent' }, //Props
-	[
-		React.createElement('div', { id: 'child1' }, [
-			//children
-			React.createElement('h1', {}, 'Hello World 1'),
-			React.createElement('h2', {}, 'How Are You? 1'),
-		]),
-		React.createElement('div', { id: 'child2' }, [
-			//children
-			React.createElement('h1', {}, 'Hello World 2'),
-			React.createElement('h2', {}, 'How Are You? 2'),
-		]),
-	],
+const heading = React.createElement(
+	'h1',
+	{ id: 'heading' },
+	'Namaste JavaScript',
 )
-
-console.log(parent) //Object
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent) //render() => converts the js object into html h1 tag
-
-// Object -> HTML
+root.render(heading)
