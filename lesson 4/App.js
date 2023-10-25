cbb97443af5a96e628fcdcadb93d6cbd521723ad
anoopraju31 <anoopraju31@gmail.com>
2023-10-25 14:16:08 +0530

    Props - Destructuring

--- lesson 4/App.js
+++ lesson 4/App.js
@@ -26,15 +26,15 @@ const styleCard = {
 	backgroundColor: '#fc4',
 }
 
-const RestaurantCard = (props) => (
+const RestaurantCard = ({ name, cuisine }) => (
 	<div className='res-card' style={styleCard}>
 		<img
 			className='res-logo'
 			src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi'
 			alt='res logo'
 		/>
-		<h3> {props?.name} </h3>
-		<h4> {props.cuisine} </h4>
+		<h3> {name} </h3>
+		<h4> {cuisine} </h4>
 		<h4> 4.4 Stars </h4>
 		<h4> 38 mins </h4>
 	</div>
