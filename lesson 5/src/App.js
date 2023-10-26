8022b5668d90adc9079f0ddde3e8a87cac16f3be
anoopraju31 <anoopraju31@gmail.com>
2023-10-26 09:09:24 +0530

    Lesson 5: RestaurantCard component file created

--- lesson 5/src/App.js
+++ lesson 5/src/App.js
@@ -910,24 +910,6 @@ const styleCard = {
 	backgroundColor: '#fc4',
 }
 
-const RestaurantCard = ({ resData }) => {
-	const { name, cloudinaryImageId, cuisines, avgRating, costForTwo } =
-		resData.info
-	return (
-		<div className='res-card' style={styleCard}>
-			<img
-				className='res-logo'
-				src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
-				alt='res logo'
-			/>
-			<h3> {name} </h3>
-			<h4> {cuisines.join(', ')} </h4>
-			<h4> {avgRating} Stars </h4>
-			<h4> {costForTwo} </h4>
-		</div>
-	)
-}
-
 const AppLayout = () => (
 	<div className='app'>
 		<Header />
--- lesson 5/src/components/Body.js
+++ lesson 5/src/components/Body.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RestaurantCard } from './'
 
 const Body = () => {
 	return (
