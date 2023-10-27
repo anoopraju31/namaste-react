277164a2ef3aeeec9b7a8f08b967338cd34f15ed
anoopraju31 <anoopraju31@gmail.com>
2023-10-27 23:27:39 +0530

    Lesson 7: Restuarant Menu - fetching menu

--- lesson 7/src/components/ResturaantMenu.js
+++ lesson 7/src/components/ResturaantMenu.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ResturaantMenu = () => {
+	useEffect(() => {
+		fetchMenu()
+	}, [])
+
+	const fetchMenu = async () => {
+		const res = await fetch(
+			'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=3241',
+		)
+		const data = await res.json()
+
+		console.log(data)
+	}
+
 	return (
 		<div>
 			<h1> Name of restuarant </h1>
