a166d7bb47129cf4c748de291cd92768dcabbccf
anoopraju31 <anoopraju31@gmail.com>
2023-10-27 21:51:11 +0530

    Lesson 7: About Page change

--- lesson 7/src/components/About.js
+++ lesson 7/src/components/About.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const About = () => {
-	return <div>About</div>
+	return (
+		<div>
+			<h1> About </h1>
+			<h2> This namaste react series </h2>
+		</div>
+	)
 }
 
 export default About
