d69d38631645be68b565f532447e689df0baf206
anoopraju31 <anoopraju31@gmail.com>
2023-10-27 21:57:20 +0530

    Lesson 7: Error Page initial & added to router

--- lesson 7/src/App.js
+++ lesson 7/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { About, Body, Contact, Header } from './components'
+import { About, Body, Contact, Error, Header } from './components'
 
 const AppLayout = () => (
 	<div className='app'>
@@ -14,6 +14,7 @@ const appRouter = createBrowserRouter([
 	{
 		path: '/',
 		element: <AppLayout />,
+		errorElement: <Error />,
 	},
 	{
 		path: '/about',
---
+++ lesson 7/src/components/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+	return (
+		<div>
+			<h1> OOP!! </h1>
+			<p> something went wrong! </p>
+		</div>
+	)
+}
+
+export default Error
