f8739987956f4cd0ce5f094a6a35c34784389e8b
anoopraju31 <anoopraju31@gmail.com>
2023-10-27 13:29:39 +0530

    Lesson 6: Login Logout button added in header

--- lesson 6/src/components/Header.js
+++ lesson 6/src/components/Header.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LOGO_URL } from '../utills/constants'
 
-const Header = () => (
-	<div className='header'>
-		<div className='logo-container'>
-			<img className='logo' src={LOGO_URL} alt='logo' />
-		</div>
+const Header = () => {
+	const [btnName, setBtnName] = useState('Login')
+	return (
+		<div className='header'>
+			<div className='logo-container'>
+				<img className='logo' src={LOGO_URL} alt='logo' />
+			</div>
 
-		<div className='nav-items'>
-			<ul>
-				<li> Home </li>
-				<li> About Us </li>
-				<li> Contact Us </li>
-				<li> Cart </li>
-			</ul>
+			<div className='nav-items'>
+				<ul>
+					<li> Home </li>
+					<li> About Us </li>
+					<li> Contact Us </li>
+					<li> Cart </li>
+					<li>
+						<button
+							onClick={() => {
+								setBtnName('Logout')
+							}}
+							className='login-button'>
+							{btnName}
+						</button>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
 
 export default Header
