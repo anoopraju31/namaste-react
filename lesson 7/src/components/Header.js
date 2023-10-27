bb6c9696139129298acaf75426922bd6d5676ea1
anoopraju31 <anoopraju31@gmail.com>
2023-10-27 22:23:12 +0530

    Lesson 7: Header links to route added

--- lesson 7/src/components/Header.js
+++ lesson 7/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { LOGO_URL } from '../utills/constants'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 	const [btnName, setBtnName] = useState('Login')
@@ -14,9 +15,15 @@ const Header = () => {
 
 			<div className='nav-items'>
 				<ul>
-					<li> Home </li>
-					<li> About Us </li>
-					<li> Contact Us </li>
+					<li>
+						<Link to='/'> Home </Link>
+					</li>
+					<li>
+						<Link to='/about'> About Us </Link>
+					</li>
+					<li>
+						<Link to='contact'> Contact Us </Link>
+					</li>
 					<li> Cart </li>
 					<li>
 						<button
