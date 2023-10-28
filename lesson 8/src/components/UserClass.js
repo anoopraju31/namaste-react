30ee94a26c5c83061a96053b05b8fb1fbe79eacb
anoopraju31 <anoopraju31@gmail.com>
2023-10-28 19:50:01 +0530

    Lesson 8: User Class Component - added

---
+++ lesson 8/src/components/UserClass.js
@@ -0,0 +1,11 @@
+class UserClass extends React.Component {
+	render() {
+		return (
+			<div className='user-card'>
+				<h2> Name: Anoop </h2>
+				<h3> Location: Kerala </h3>
+				<h4> Contact Anoop2019 </h4>
+			</div>
+		)
+	}
+}
