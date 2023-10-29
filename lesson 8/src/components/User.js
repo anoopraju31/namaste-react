98b6654f36ecb49e7a39882208b962ded8001a94
anoopraju31 <anoopraju31@gmail.com>
2023-10-29 14:39:07 +0530

    Lesson 8: create State Variable in react class components

--- lesson 8/src/components/User.js
+++ lesson 8/src/components/User.js
@@ -1,6 +1,12 @@
+import { useState } from 'react'
+
 const User = ({ name, location, contact }) => {
+	const [count, setCount] = useState(0)
+	const [count2, setCount2] = useState(0)
 	return (
 		<div className='user-card'>
+			<h1> Count: {count} </h1>
+			<h1> Count2: {count2} </h1>
 			<h2> Name: {name} </h2>
 			<h3> Location: {location} </h3>
 			<h4> Contact: {contact} </h4>
--- lesson 8/src/components/UserClass.js
+++ lesson 8/src/components/UserClass.js
@@ -4,12 +4,20 @@ class UserClass extends React.Component {
 	constructor(props) {
 		super(props)
 		// console.log(props)
+
+		this.state = {
+			count: 0,
+			count2: 2,
+		}
 	}
 
 	render() {
 		const { name, location, contact } = this.props
+		const { count, count2 } = this.state
 		return (
 			<div className='user-card'>
+				<h1> Count: {count} </h1>
+				<h1> Count2: {count2} </h1>
 				<h2> Name: {name} </h2>
 				<h3> Location: {location} </h3>
 				<h4> Contact: {contact} </h4>
