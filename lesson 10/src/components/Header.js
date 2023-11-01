cfb52ca51a1ddaacd16733d873eef13ebb329f14
anoopraju31 <anoopraju31@gmail.com>
2023-11-01 14:52:04 +0530

    Lesson 10: header styled with tailwind

--- lesson 10/src/components/Header.js
+++ lesson 10/src/components/Header.js
@@ -5,18 +5,22 @@ import { useOnlineStatus } from '../hooks'
 
 const Header = () => {
 	const [btnName, setBtnName] = useState('Login')
+	const [navOpen, setNavOpen] = useState(false)
 	const onlineStatus = useOnlineStatus()
 
 	// console.log('Header Component!')
 
 	return (
-		<div className='header'>
+		<div className='container mx-auto flex justify-between'>
 			<div className='logo-container'>
-				<img className='logo' src={LOGO_URL} alt='logo' />
+				<img className='h-14  object-contain' src={LOGO_URL} alt='logo' />
 			</div>
 
-			<div className='nav-items'>
-				<ul>
+			<div className='relative flex items-center'>
+				<ul
+					className={`w-72 lg:w-full absolute lg:static ${
+						navOpen ? 'top-16 right-0' : '-top-[100vh] right-0'
+					} flex flex-col lg:flex-row gap-6 p-6 rounded-lg bg-green-500 lg:bg-transparent transition-all duration-500 ease-in-out`}>
 					<li>Online Status: {onlineStatus ? '✅' : '❌'} </li>
 					<li>
 						<Link to='/'> Home </Link>
@@ -41,6 +45,10 @@ const Header = () => {
 						</button>
 					</li>
 				</ul>
+
+				<button
+					onClick={() => setNavOpen((prev) => !prev)}
+					className='w-10 h-10 rounded-md bg-slate-500 flex lg:hidden justify-center items-center'></button>
 			</div>
 		</div>
 	)
