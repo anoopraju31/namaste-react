bbd2447c30d5df33da4a75dc6a90afd426c9c681
anoopraju31 <anoopraju31@gmail.com>
2023-11-02 14:30:08 +0530

    Lesson 11: Restaurant Menu - Heading styled

--- lesson 11/src/components/ResturaantMenu.js
+++ lesson 11/src/components/ResturaantMenu.js
@@ -4,6 +4,7 @@ import { useRestuarantInfo } from '../hooks'
 import { IMAGE_CDN_URL } from '../utills/constants'
 import FoodCard from './FoodCard'
 import Shimmer from './Shimmer'
+import { AiFillStar } from 'react-icons/ai'
 
 const ResturaantMenu = () => {
 	const { id } = useParams()
@@ -11,28 +12,46 @@ const ResturaantMenu = () => {
 
 	if (restuarantInfo === null) return <Shimmer />
 
-	const { name, cuisines, cloudinaryImageId, costForTwo, avgRating } =
+	const { name, cuisines, cloudinaryImageId, locality, costForTwo, avgRating } =
 		restuarantInfo?.cards[0]?.card?.card?.info
 	const { itemCards } =
 		restuarantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
 			?.card
+	const categories =
+		restuarantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+			(c) =>
+				c.card?.card?.['@type'].includes(
+					'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory',
+				),
+		)
+
+	console.log(categories)
 
 	return (
-		<div>
-			<img src={IMAGE_CDN_URL + cloudinaryImageId} alt='' />
-			<h1> {name} </h1>
-			<p> {cuisines.join(', ')} </p>
-			<p> {costForTwo} </p>
-			<p> {avgRating} </p>
-			<h2> Menu </h2>
-			<h2> Recommended </h2>
-			<ul className='food-container'>
+		<div className='max-w-5xl mx-auto px-6'>
+			<div className='py-6 flex justify-between border-b border-b-gray-300 mb-10'>
+				<div className='flex flex-col text-gray-600 font-bold text-sm gap-1'>
+					<h1 className='text-3xl text-slate-800'> {name} </h1>
+
+					<p> {cuisines.join(', ')} </p>
+					<p> {locality} </p>
+				</div>
+
+				<div className=''>
+					<div className='flex gap-2 items-center text-lg text-green-500 font-medium'>
+						<AiFillStar />
+						<p> {avgRating} </p>
+					</div>
+				</div>
+			</div>
+
+			{/* <ul className='food-container'>
 				{itemCards?.map((item) => (
 					<li key={item?.card?.info?.id}>
 						<FoodCard food={item?.card?.info} />
 					</li>
 				))}
-			</ul>
+			</ul> */}
 		</div>
 	)
 }
