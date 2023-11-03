208f197298510e2098179ef09cf5689e40d09bdd
anoopraju31 <anoopraju31@gmail.com>
2023-11-03 13:17:59 +0530

    Lesson 11: Restaurant Menu - Add button added to Food Card

--- lesson 11/src/components/FoodCard.js
+++ lesson 11/src/components/FoodCard.js
@@ -5,7 +5,7 @@ const FoodCard = ({ food, isBorderBottom }) => {
 	const price = food?.defaultPrice ? food?.defaultPrice : food?.price
 	return (
 		<li
-			className={`flex justify-between items-center px-6 py-3 sm:px-0 gap-4 ${
+			className={`group flex justify-between items-center px-6 py-3 sm:px-0 gap-4 ${
 				isBorderBottom && ' border-b border-b-gray-300'
 			}`}>
 			<div className='flex-1'>
@@ -14,12 +14,18 @@ const FoodCard = ({ food, isBorderBottom }) => {
 				<p className='text-xs my-2 text-gray-500'> {food?.description} </p>
 			</div>
 
-			<div className='w-28'>
+			<div className='w-28 relative'>
 				<img
 					className='rounded-lg'
 					src={IMAGE_CDN_URL + food?.imageId}
 					alt=''
 				/>
+
+				<div className='hidden group-hover:flex absolute top-0 left-0 right-0 bottom-0 bg-black/5 justify-center items-center'>
+					<button className='bg-white text-xs flex justify-center items-center text-green-500 font-bold uppercase px-3 py-1 rounded-md'>
+						add
+					</button>
+				</div>
 			</div>
 		</li>
 	)
