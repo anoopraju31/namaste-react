5fbced6d5560b77473c4a16242da96ae423b5e5e
anoopraju31 <anoopraju31@gmail.com>
2023-11-06 21:04:26 +0530

    Lesson 12: CartSlice items change

--- lesson 12/src/reducers/cartSlice.js
+++ lesson 12/src/reducers/cartSlice.js
@@ -4,17 +4,33 @@ const cartSlice = createSlice({
 	name: 'cart',
 	initialState: {
 		items: [],
+		quantity: 0,
 	},
 	reducers: {
 		addItem: (state, action) => {
 			// Mutating the state here
-			state.items.push(action.payload)
+			const index = state.items.findIndex(
+				(item) => item.food.id === action.payload.id,
+			)
+
+			if (index === -1) {
+				state.items.push({
+					food: action.payload,
+					quantity: 1,
+				})
+			} else {
+				state.items[index].quantity += 1
+			}
+
+			state.quantity += 1
 		},
 		removeItem: (state) => {
+			state.quantity -= 1
 			state.items.pop()
 		},
 		clearCart: (state) => {
 			state.items.length = 0
+			state.quantity = 0
 		},
 	},
 })
