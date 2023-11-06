ff8c27c995a397e4d1ac49461e7dc50bd89f8070
anoopraju31 <anoopraju31@gmail.com>
2023-11-06 13:43:27 +0530

    Lesson 12: Cart slice added

--- lesson 12/src/reducers/store.js
+++ lesson 12/src/reducers/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartSlice from './cartSlice'
 
-const store = configureStore()
+const store = configureStore({
+	cart: cartSlice,
+})
 
 export default store
