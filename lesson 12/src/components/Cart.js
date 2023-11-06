2fa3d730091002cce1b3e6d0ec02a986b39b0aa7
anoopraju31 <anoopraju31@gmail.com>
2023-11-06 20:45:15 +0530

    Lesson 12: Cart Item Component - intial

--- lesson 12/src/components/Cart.js
+++ lesson 12/src/components/Cart.js
@@ -1,5 +1,23 @@
+import { useSelector } from 'react-redux'
+import FoodCard from './FoodCard'
+import CartItem from './CartItem'
+
 const Cart = () => {
-	return <div className=''> Cart </div>
+	const cartItems = useSelector((state) => state.cart.items)
+
+	return (
+		<div className=''>
+			<h1 className='text-2xl font-bold text-center mb-4'> Cart </h1>
+
+			<div className=''>
+				<ul className='sm:px-6 flex flex-col gap-4'>
+					{cartItems?.map((item, idx) => (
+						<CartItem key={item?.id} />
+					))}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default Cart
