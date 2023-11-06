fef249c4fb9de9288233129e0d4053a9285ff21e
anoopraju31 <anoopraju31@gmail.com>
2023-11-06 21:16:24 +0530

    Lesson 12: Cart Item component - styled

--- lesson 12/src/components/Cart.js
+++ lesson 12/src/components/Cart.js
@@ -1,18 +1,22 @@
 import { useSelector } from 'react-redux'
-import FoodCard from './FoodCard'
 import CartItem from './CartItem'
 
 const Cart = () => {
 	const cartItems = useSelector((state) => state.cart.items)
 
 	return (
-		<div className=''>
+		<div className='max-w-3xl mx-auto'>
 			<h1 className='text-2xl font-bold text-center mb-4'> Cart </h1>
 
 			<div className=''>
 				<ul className='sm:px-6 flex flex-col gap-4'>
 					{cartItems?.map((item, idx) => (
-						<CartItem key={item?.id} />
+						<CartItem
+							key={idx}
+							food={item.food}
+							quantity={item.quantity}
+							isBorderBottom={cartItems.length - 1 !== idx}
+						/>
 					))}
 				</ul>
 			</div>
--- lesson 12/src/components/CartItem.js
+++ lesson 12/src/components/CartItem.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import { IMAGE_CDN_URL } from '../utills/constants'
 
-const CartItem = () => {
-	return <div>CartItem</div>
+const CartItem = ({ food, quantity, isBorderBottom }) => {
+	const price = food?.defaultPrice ? food?.defaultPrice : food?.price
+
+	return (
+		<li
+			className={`group flex justify-between px-6 py-3 sm:px-0 gap-4 ${
+				isBorderBottom && ' border-b border-b-gray-300'
+			}`}>
+			<div className='w-28'>
+				<img
+					className='rounded-lg'
+					src={IMAGE_CDN_URL + food?.imageId}
+					alt=''
+				/>
+			</div>
+			<div className='flex-1 text-xl flex flex-col justify-start'>
+				<h3 className='font-medium mb-[2px]'> {food?.name} </h3>
+				<p className='font-medium'> ₹{price / 100} </p>
+			</div>
+
+			<div className='text-xl flex gap-4 justify-center items-center'>
+				<button className='w-9 h-9 bg-gray-200 flex justify-center items-center rounded-md'>
+					<AiOutlineMinus />
+				</button>
+				<p> {quantity} </p>
+				<button className='w-9 h-9 bg-gray-200 flex justify-center items-center rounded-md'>
+					<AiOutlinePlus />{' '}
+				</button>
+			</div>
+		</li>
+	)
 }
 
 export default CartItem
