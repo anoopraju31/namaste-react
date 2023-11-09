b604bf9aa0adda1262e45f54ed4e1d93fbe43b4c
anoopraju31 <anoopraju31@gmail.com>
2023-11-09 12:53:16 +0530

    Lesson 13: Contact Page - grouping test cases with describe

--- lesson 13/src/components/__tests__/Contact.test.js
+++ lesson 13/src/components/__tests__/Contact.test.js
@@ -2,41 +2,43 @@ import { render, screen } from '@testing-library/react'
 import Contact from '../Contact'
 import '@testing-library/jest-dom'
 
-test('Should load contact us component', () => {
-	render(<Contact />)
+describe('Contact Page test cases', () => {
+	test('Should load contact us component', () => {
+		render(<Contact />)
 
-	const heading = screen.getByRole('heading')
+		const heading = screen.getByRole('heading')
 
-	// Assertion
-	expect(heading).toBeInTheDocument()
-})
+		// Assertion
+		expect(heading).toBeInTheDocument()
+	})
 
-test('Should load button inside Contact component', () => {
-	render(<Contact />)
+	test('Should load button inside Contact component', () => {
+		render(<Contact />)
 
-	const button = screen.getByRole('button')
+		const button = screen.getByRole('button')
 
-	// Assertion
-	expect(button).toBeInTheDocument()
-})
+		// Assertion
+		expect(button).toBeInTheDocument()
+	})
 
-test('Should load input name inside Contact component', () => {
-	render(<Contact />)
+	test('Should load input name inside Contact component', () => {
+		render(<Contact />)
 
-	const inputName = screen.getByPlaceholderText('name')
+		const inputName = screen.getByPlaceholderText('name')
 
-	// Assertion
-	expect(inputName).toBeInTheDocument()
-})
+		// Assertion
+		expect(inputName).toBeInTheDocument()
+	})
 
-test('Should load 2 input boxes inside Contact component', () => {
-	render(<Contact />)
+	test('Should load 2 input boxes inside Contact component', () => {
+		render(<Contact />)
 
-	// Querying
-	const inputBoxes = screen.getAllByRole('textbox')
+		// Querying
+		const inputBoxes = screen.getAllByRole('textbox')
 
-	// console.log(inputBoxes.length)
+		// console.log(inputBoxes.length)
 
-	// Assertion
-	expect(inputBoxes.length).toBe(2)
+		// Assertion
+		expect(inputBoxes.length).toBe(2)
+	})
 })
