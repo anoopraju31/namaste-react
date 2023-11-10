540c0be29b30a149bf1fa40e4667b45fb9b1bd2c
anoopraju31 <anoopraju31@gmail.com>
2023-11-10 21:13:33 +0530

    Lesson 13: Restaurant Card Testing - RestaurantCard component with veg label props data

--- lesson 13/src/components/__tests__/RestaurantCard.test.js
+++ lesson 13/src/components/__tests__/RestaurantCard.test.js
@@ -1,11 +1,11 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import RestaurantCard from '../RestaurantCard'
+import { render, screen } from '@testing-library/react'
+import RestaurantCard, { withVegLabel } from '../RestaurantCard'
 import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
-import { data } from '../mocks/restaurantData'
+import { data, vegRestaurant } from '../mocks/restaurantData'
 
 describe('Restarant Card Component Testing', () => {
-	it('Should render RestauranCard component with props data', () => {
+	it('Should render Restaurant Card component with props data', () => {
 		render(
 			<BrowserRouter>
 				<RestaurantCard resData={data} />
@@ -16,6 +16,29 @@ describe('Restarant Card Component Testing', () => {
 		const restaurantCuisines = screen.getByText(data.info.cuisines.join(', '))
 		const restaurantRatings = screen.getByText(data.info.avgRating + ' Stars')
 		const costOfTwo = screen.getByText(data.info.costForTwo)
+
+		expect(restaurantName).toBeInTheDocument()
+		expect(restaurantCuisines).toBeInTheDocument()
+		expect(restaurantRatings).toBeInTheDocument()
+		expect(costOfTwo).toBeInTheDocument()
+	})
+
+	it('Should render Restaurant Card component with Veg Label props data', () => {
+		const VegRestaurantCard = withVegLabel(RestaurantCard)
+		render(
+			<BrowserRouter>
+				<VegRestaurantCard resData={vegRestaurant} />
+			</BrowserRouter>,
+		)
+
+		const restaurantName = screen.getByText(vegRestaurant.info.name)
+		const restaurantCuisines = screen.getByText(
+			vegRestaurant.info.cuisines.join(', '),
+		)
+		const restaurantRatings = screen.getByText(
+			vegRestaurant.info.avgRating + ' Stars',
+		)
+		const costOfTwo = screen.getByText(vegRestaurant.info.costForTwo)
 
 		expect(restaurantName).toBeInTheDocument()
 		expect(restaurantCuisines).toBeInTheDocument()
--- lesson 13/src/components/mocks/restaurantData.js
+++ lesson 13/src/components/mocks/restaurantData.js
@@ -108,3 +108,114 @@ export const data = {
 	},
 	widgetId: 'collectionV5RestaurantListWidget_SimRestoRelevance_food_seo',
 }
+
+export const vegRestaurant = {
+	info: {
+		id: '534235',
+		name: 'Cheesecake & Co.',
+		cloudinaryImageId: '40b0a201eadbec469c2f9a06d5e7c722',
+		locality: '6th Block',
+		areaName: 'Koramangala',
+		costForTwo: '₹250 for two',
+		cuisines: ['Bakery'],
+		avgRating: 4.5,
+		veg: true,
+		feeDetails: {
+			restaurantId: '534235',
+			fees: [
+				{
+					name: 'BASE_DISTANCE',
+					fee: 7400,
+				},
+				{
+					name: 'BASE_TIME',
+				},
+				{
+					name: 'ANCILLARY_SURGE_FEE',
+				},
+			],
+			totalFee: 7400,
+		},
+		parentId: '387417',
+		avgRatingString: '4.5',
+		totalRatingsString: '1K+',
+		sla: {
+			deliveryTime: 34,
+			lastMileTravel: 6.2,
+			serviceability: 'SERVICEABLE',
+			slaString: '34 mins',
+			lastMileTravelString: '6.2 km',
+			iconType: 'ICON_TYPE_EMPTY',
+		},
+		availability: {
+			nextCloseTime: '2023-11-11 00:00:00',
+			opened: true,
+		},
+		badges: {
+			imageBadges: [
+				{
+					imageId: 'newg.png',
+					description: 'Gourmet',
+				},
+				{
+					imageId: 'v1695133679/badges/Pure_Veg111.png',
+					description: 'pureveg',
+				},
+			],
+		},
+		isOpen: true,
+		type: 'F',
+		badgesV2: {
+			entityBadges: {
+				imageBased: {
+					badgeObject: [
+						{
+							attributes: {
+								description: 'Gourmet',
+								imageId: 'newg.png',
+							},
+						},
+						{
+							attributes: {
+								description: 'pureveg',
+								imageId: 'v1695133679/badges/Pure_Veg111.png',
+							},
+						},
+					],
+				},
+				textBased: {},
+				textExtendedBadges: {},
+			},
+		},
+		aggregatedDiscountInfoV3: {
+			header: '50% OFF',
+			subHeader: 'UPTO ₹100',
+		},
+		orderabilityCommunication: {
+			title: {},
+			subTitle: {},
+			message: {},
+			customIcon: {},
+		},
+		differentiatedUi: {
+			displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+			differentiatedUiMediaDetails: {
+				mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+				lottie: {},
+				video: {},
+			},
+		},
+		reviewsSummary: {},
+		displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+		restaurantOfferPresentationInfo: {},
+	},
+	analytics: {
+		context: 'seo-data-9d165898-b5d9-46e2-821d-a0fb6b625b55',
+	},
+	cta: {
+		link: 'https://www.swiggy.com/restaurants/cheesecake-and-co-6th-block-koramangala-bangalore-534235',
+		text: 'RESTAURANT_MENU',
+		type: 'WEBLINK',
+	},
+	widgetId: 'collectionV5RestaurantListWidget_SimRestoRelevance_food_seo',
+}
