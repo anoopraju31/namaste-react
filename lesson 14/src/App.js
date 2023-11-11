6eb6c419067ffabac6a7c731620b06809c979a43
anoopraju31 <anoopraju31@gmail.com>
2023-11-11 17:37:01 +0530

    Lesson 14: Netflix GPT - tailwindcss initialized

--- lesson 14/src/App.js
+++ lesson 14/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const App = () => {
-	return <div>Namaste Everyone, let's build Netflix GPT.</div>
+	return (
+		<div className='text-3xl font-bold underline'>
+			Namaste Everyone, let's build Netflix GPT.
+		</div>
+	)
 }
 
 export default App
