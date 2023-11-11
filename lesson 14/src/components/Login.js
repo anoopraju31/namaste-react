9ef2c887f5ba1abb7a0aeb730ce80d24e2dd0bf0
anoopraju31 <anoopraju31@gmail.com>
2023-11-11 18:57:04 +0530

    Lesson 14: Netflix Gpt - Header Component - logo added

--- lesson 14/src/components/Login.js
+++ lesson 14/src/components/Login.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
-	return <div>Login</div>
+	return (
+		<div>
+			<Header />
+
+			<div className=''>
+				<img
+					src='https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+					alt=''
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Login
