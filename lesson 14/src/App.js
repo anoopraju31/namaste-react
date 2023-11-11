e18678857ccfdaa27ea7b274df845baca72aa5e2
anoopraju31 <anoopraju31@gmail.com>
2023-11-11 18:13:24 +0530

    Lesson 14: Netflix Gpt - Body Component - initial

--- lesson 14/src/App.js
+++ lesson 14/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import Body from './components/Body'
 
 const App = () => {
-	return (
-		<div className='text-3xl font-bold underline'>
-			Namaste Everyone, let's build Netflix GPT.
-		</div>
-	)
+	return <Body />
 }
 
 export default App
