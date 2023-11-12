be2e05211d7009411528378878cc538241448ce7
anoopraju31 <anoopraju31@gmail.com>
2023-11-12 17:38:42 +0530

    Lesson 15: Movie Slice created and added to store

--- lesson 15/src/reducers/store.js
+++ lesson 15/src/reducers/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import movieReducer from './movieSlices'
 
 const store = configureStore({
 	reducer: {
 		user: userReducer,
+		movies: movieReducer,
 	},
 })
 
