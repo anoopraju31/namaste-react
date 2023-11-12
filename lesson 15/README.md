96aa57f512ad1878ec0e6abf1ee041a7fdf5393e
anoopraju31 <anoopraju31@gmail.com>
2023-11-12 17:35:00 +0530

    Lesson 15: Browse Page - get data from TMDB now playing API

--- lesson 15/README.md
+++ lesson 15/README.md
@@ -18,6 +18,8 @@
 - BugFix: if user is not logged in Redirect /browse to login page and vice-versa 
 - Unsubscribe to the onAuthStateChanged callback
 - Add hardcoded values to the constants file
+- Register TMDB API & create an app & get access token
+- Get data from TMDB now playing movie list API
 
 ## Features
 - Login / Sign Up
--- lesson 15/src/components/Browse.js
+++ lesson 15/src/components/Browse.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utills/constants'
 
 const Browse = () => {
+	useEffect(() => {
+		getNowPlayingMovies()
+	}, [])
+
+	const getNowPlayingMovies = async () => {
+		const res = await fetch(
+			'https://api.themoviedb.org/3/movie/now_playing?page=1',
+			API_OPTIONS,
+		)
+		const data = await res.json()
+
+		console.log(data)
+	}
+
 	return (
 		<main>
 			<Header />
