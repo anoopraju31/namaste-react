68ff38940ddd130c49c515bc1d60c4b89d8658a6
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 19:23:14 +0530

    Lesson 16: GPTSearch - language change

--- lesson 16/src/components/GPTSearchBar.js
+++ lesson 16/src/components/GPTSearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import languages from '../utills/languageConstants'
 
 const GPTSearchBar = () => {
 	return (
@@ -7,13 +8,12 @@ const GPTSearchBar = () => {
 				<input
 					type='text'
 					className='w-full max-w-2xl p-3 sm:p-4 rounded-tl-xl rounded-bl-xl text-gray-900 text-sm sm:text-base sm:font-medium outline-none'
-					placeholder='What would you like to watch today?'
+					placeholder={languages['hi'].placeholder}
 				/>
 				<button
 					type='submit'
 					className='py-3 sm:py-4 px-4 sm:px-6 bg-red-600 text-white rounded-tr-xl rounded-br-xl text-sm sm:text-base sm:font-medium'>
-					{' '}
-					Search{' '}
+					{languages['hi'].search}
 				</button>
 			</form>
 		</div>
