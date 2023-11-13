26e956442b1564f16b8e3b75148bbc5d8846adf6
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 23:14:42 +0530

    Lesson 16: GPT Movie Suggestions - styled

--- lesson 16/src/components/GPTMovieSuggestions.js
+++ lesson 16/src/components/GPTMovieSuggestions.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieCard from './MovieCard'
 
 const GPTMovieSuggestions = () => {
-	return <div>GPTMovieSuggestions</div>
+	const { moviesNames, movieResults } = useSelector((state) => state.gpt)
+
+	if (!moviesNames) return null
+
+	return (
+		<section className='max-w-7xl mx-auto p-4 mt-16 flex flex-wrap justify-between gap-4'>
+			{movieResults?.map((movie) => (
+				<MovieCard key={movie.id} poster={movie?.poster_path} />
+			))}
+		</section>
+	)
 }
 
 export default GPTMovieSuggestions
