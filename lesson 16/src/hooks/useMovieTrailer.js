0ba04181cd8512f1d6f6f6c32f4ea828c8379a4c
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 01:02:47 +0530

    Lesson 16: Custom Hook - useMovieTrailer - memorization added

--- lesson 16/src/hooks/useMovieTrailer.js
+++ lesson 16/src/hooks/useMovieTrailer.js
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { API_OPTIONS, TMDB_MOVIE_VIDEO_API } from '../utills/constants'
 import { addTrailerVideo } from '../reducers/movieSlices'
 
 const useMovieTrailer = (movieId) => {
+	const trailerVideo = useSelector((state) => state.movies.trailerVideo)
 	const dispatch = useDispatch()
 
 	const getMovieVideos = async (movieId) => {
+		if (trailerVideo) return
+
 		const res = await fetch(TMDB_MOVIE_VIDEO_API(movieId), API_OPTIONS)
 		const data = await res.json()
 		const filteredData = data?.results?.filter(
