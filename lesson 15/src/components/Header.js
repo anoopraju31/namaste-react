715fdc71d965d717f1c50550d8a498e8c06d2d43
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 10:32:17 +0530

    Lesson 15: Header Component - responsive

--- lesson 15/src/components/Header.js
+++ lesson 15/src/components/Header.js
@@ -34,11 +34,15 @@ const Header = () => {
 	return (
 		<header className='absolute w-full max-w-[1536px] mx-auto px-0 sm:px-8 py-2 bg-gradient-to-b from-black z-50 flex flex-col md:flex-row justify-between'>
 			<div className=''>
-				<img className='w-36 sm:w-44' src={LOGO_IMG} alt='logo' />
+				<img
+					className='w-28 sm:w-32 md:w-36 lg:w-44'
+					src={LOGO_IMG}
+					alt='logo'
+				/>
 			</div>
 
 			{user && (
-				<div className='flex'>
+				<div className='hidden md:flex'>
 					<img className='w-12 h-12' src={user?.photoURL} alt='user avatar' />
 
 					<button onClick={handleSignOut} className='font-bold text-white'>
