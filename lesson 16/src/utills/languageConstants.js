ea9840893106cefeeed6c48b1c5d00de3017b8ae
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 19:19:33 +0530

    Lesson 16: Language Constants - created

---
+++ lesson 16/src/utills/languageConstants.js
@@ -0,0 +1,20 @@
+const languages = {
+	en: {
+		search: 'Search',
+		placeholder: 'What would you like to watch today?',
+	},
+	hi: {
+		search: 'खोज',
+		placeholder: 'आज आप क्या देखना चाहेंगे?',
+	},
+	ml: {
+		search: 'തിരയുക',
+		placeholder: 'ഇന്ന് നിങ്ങൾ എന്താണ് കാണാൻ ആഗ്രഹിക്കുന്നത്?',
+	},
+	es: {
+		search: 'buscar',
+		placeholder: '¿Qué te gustaría ver hoy?',
+	},
+}
+
+export default languages
