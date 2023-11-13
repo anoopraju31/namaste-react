7520408b36ab63923fa869360947dfd14108f555
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 18:45:27 +0530

    Lesson 16: GPT Search Bar - styled

--- lesson 16/src/components/GPTSearch.js
+++ lesson 16/src/components/GPTSearch.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import GPTSearchBar from './GPTSearchBar'
+import GPTMovieSuggestions from './GPTMovieSuggestions'
+import { LOGIN_BG } from '../utills/constants'
 
 const GPTSearch = () => {
 	return (
-		<div>
-			<GPTSearchBar />
+		<div className='relative'>
+			<section className='absolute w-full h-screen'>
+				<img
+					className='w-full h-full object-cover'
+					src={LOGIN_BG}
+					alt='login background'
+				/>
+			</section>
+			<section className='relative w-full min-h-screen z-20 bg-black/50'>
+				<GPTSearchBar />
+				<GPTMovieSuggestions />
+			</section>
 		</div>
 	)
 }
