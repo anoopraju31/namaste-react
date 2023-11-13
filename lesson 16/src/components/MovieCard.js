b9a36dccbf102a715c16a284e477b65df9781c92
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 18:54:58 +0530

    Lesson 16: Movie Card - refactor

--- lesson 16/src/components/MovieCard.js
+++ lesson 16/src/components/MovieCard.js
@@ -3,7 +3,7 @@ import { IMG_CDN_URL } from '../utills/constants'
 const MovieCard = ({ poster }) => {
 	return (
 		<div className='w-36 md:w-48 flex-shrink-0'>
-			<img src={IMG_CDN_URL + poster} alt='' />
+			<img className='rounded-xl' src={IMG_CDN_URL + poster} alt='' />
 		</div>
 	)
 }
