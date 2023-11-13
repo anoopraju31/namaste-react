094d0824624ed13c7e2f24967ced52497288d0b6
anoopraju31 <anoopraju31@gmail.com>
2023-11-13 23:28:01 +0530

    Lesson 16: GPT Movie Suggestions - search text added as heading

--- lesson 16/src/components/GPTMovieSuggestions.js
+++ lesson 16/src/components/GPTMovieSuggestions.js
@@ -2,15 +2,23 @@ import { useSelector } from 'react-redux'
 import MovieCard from './MovieCard'
 
 const GPTMovieSuggestions = () => {
-	const { moviesNames, movieResults } = useSelector((state) => state.gpt)
+	const { searchText, moviesNames, movieResults } = useSelector(
+		(state) => state.gpt,
+	)
 
 	if (!moviesNames) return null
 
 	return (
-		<section className='w-fit mx-auto p-4 mt-16 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4'>
-			{movieResults?.map((movie) => (
-				<MovieCard key={movie.id} poster={movie?.poster_path} />
-			))}
+		<section className='p-4 md:mt-10 lg:mt-16'>
+			<h1 className='mb-6 text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-white'>
+				{' '}
+				Search result for "{searchText}"{' '}
+			</h1>
+			<div className='w-fit mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4'>
+				{movieResults?.map((movie) => (
+					<MovieCard key={movie.id} poster={movie?.poster_path} />
+				))}
+			</div>
 		</section>
 	)
 }
