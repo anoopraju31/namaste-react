ebb43f8400411207f57739f53fe7fdd4f4e9f542
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 09:37:03 +0530

    Lesson 16: React Router - Change

--- lesson 16/src/App.js
+++ lesson 16/src/App.js
@@ -1,11 +1,35 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Body from './components/Body'
+import Login from './components/Login'
+import Browse from './components/Browse'
+import GPTSearch from './components/GPTSearch'
 import store from './reducers/store'
 
 const App = () => {
+	const appRouter = createBrowserRouter([
+		{
+			path: '/',
+			element: <Body />,
+			children: [
+				{
+					path: '/',
+					element: <Login />,
+				},
+				{
+					path: '/browse',
+					element: <Browse />,
+				},
+				{
+					path: '/search',
+					element: <GPTSearch />,
+				},
+			],
+		},
+	])
 	return (
 		<Provider store={store}>
-			<Body />
+			<RouterProvider router={appRouter} />
 		</Provider>
 	)
 }
--- lesson 16/src/components/Body.js
+++ lesson 16/src/components/Body.js
@@ -1,20 +1,13 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Browse from './Browse'
-import Login from './Login'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
 
 const Body = () => {
-	const appRouter = createBrowserRouter([
-		{
-			path: '/',
-			element: <Login />,
-		},
-		{
-			path: '/browse',
-			element: <Browse />,
-		},
-	])
-
-	return <RouterProvider router={appRouter} />
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	)
 }
 
 export default Body
