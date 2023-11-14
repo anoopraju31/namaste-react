ccea54c45401036cdd104c306f30fbd4d6215ca8
anoopraju31 <anoopraju31@gmail.com>
2023-11-14 09:41:48 +0530

    Lesson 16: Browse Page - refactor

--- lesson 16/src/components/Browse.js
+++ lesson 16/src/components/Browse.js
@@ -1,33 +1,20 @@
-import { useSelector } from 'react-redux'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import useTopRatedMovies from '../hooks/useTopRatedMovies'
 import useTrendingMovies from '../hooks/useTrendingMovies'
 import usePopularMovies from '../hooks/usePopularMovies'
-import Header from './Header'
 import MainContainer from './MainContainer'
 import SecondaryContainer from './SecondaryContainer'
-import GPTSearch from './GPTSearch'
 
 const Browse = () => {
-	const showGPTSearch = useSelector((state) => state?.gpt?.showGPTSearch)
-
 	useNowPlayingMovies()
 	usePopularMovies()
 	useTopRatedMovies()
 	useTrendingMovies()
 
 	return (
-		<main className='bg-black min-h-screen'>
-			<Header />
-
-			{showGPTSearch ? (
-				<GPTSearch />
-			) : (
-				<div className='max-w-[1536px] mx-auto'>
-					<MainContainer />
-					<SecondaryContainer />
-				</div>
-			)}
+		<main className='max-w-[1536px] min-h-screen mx-auto bg-black'>
+			<MainContainer />
+			<SecondaryContainer />
 		</main>
 	)
 }
